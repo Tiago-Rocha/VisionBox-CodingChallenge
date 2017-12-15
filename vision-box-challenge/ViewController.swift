000e6157f32116a6b89cf48bcd7064ce6d4af2dd
Tiago Rocha <tiago.aziz@gmail.com>
2017-12-15 11:31:59 -0100

    Initial Commit

---
+++ vision-box-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-box-challenge
+//
+//  Created by Tiago Rocha on 15/12/2017.
+//  Copyright © 2017 tiagorocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ vision-box-challengeTests/vision_box_challengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  vision_box_challengeTests.swift
+//  vision-box-challengeTests
+//
+//  Created by Tiago Rocha on 15/12/2017.
+//  Copyright © 2017 tiagorocha. All rights reserved.
+//
+
+import XCTest
+@testable import vision_box_challenge
+
+class vision_box_challengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
